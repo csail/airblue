b3434f28981495c93a4ebb5ed34065729e34423f
Elliott Fleming <kermin.fleming@intel.com>
2010-05-20 17:21:20 +0000

    added invocation script

---
+++ scripts/invocation/soft_phy/soft_phy_bucket.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# determine awb directory
+
+$working_dir = `awb-resolver scripts/aggregation/soft_phy/`;
+chomp($working_dir);
+
+print "working dir is $working_dir\n";
+
+`regression.launcher --package=ofdm --runtype=soft_phy --runcmds="\\"-completioncmd=$working_dir/aggregate-ber-bins.pl $working_dir/plot-ber.gp\\""`;
+
