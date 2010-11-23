3407fd647057415000633609de9d1a7f0220ace7
challen <challen>
2010-11-23 18:50:14 +0000

    Add default decimation, remove printf.

--- scripts/traces/viewiq.py
+++ scripts/traces/viewiq.py
@@ -5,7 +5,7 @@ import numpy
 import matplotlib.pyplot as plt
 
 parser = OptionParser()
-parser.add_option("-d", "--decimation", dest="decimation", type=int, default=1, help="decimation rate (default 1)")
+parser.add_option("-d", "--decimation", dest="decimation", type=int, default=1000, help="decimation rate (default 1000)")
 parser.add_option("-s", "--skip", dest="skip", type=float, default=0.0, help="initial seconds to skip (default 0.1)")
 (options, args) = parser.parse_args()
 
@@ -18,7 +18,6 @@ measurements = readdata.shape[0] / options.decimation
 skip = (options.skip * 20000000) / options.decimation
 magnitudes = numpy.empty((measurements,), dtype=numpy.uint16)
 for i in numpy.arange(measurements):
-  print 'rel '  + str(readdata[i * options.decimation]["real"]) + ' img: ' + str( readdata[i * options.decimation]["imag"])
   magnitudes[i] = numpy.sqrt(readdata[i * options.decimation]["real"]**2 + readdata[i * options.decimation]["imag"]**2)
 del(readdata)
 plt.plot(magnitudes)
