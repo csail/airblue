000cd4afa38980a7724e12373eda27a2579fb115
Elliott Fleming <kermin.fleming@intel.com>
2011-04-12 23:29:05 +0000

    Packet Gen and packet check drivers added

---
+++ modules/airblue/lib/Drivers/TXPacketGen/tx-packet-gen.cpp
@@ -0,0 +1,43 @@
+
+#include "asim/provides/virtual_platform.h"
+#include "asim/provides/connected_application.h"
+#include "asim/provides/airblue_driver_application.h"
+#include "asim/provides/airblue_environment.h"
+
+using namespace std;
+ 
+// constructor
+AIRBLUE_DRIVER_CLASS::AIRBLUE_DRIVER_CLASS(PLATFORMS_MODULE p) :
+    DRIVER_MODULE_CLASS(p)
+{
+  packetCheckStub = new PACKETCHECKRRR_CLIENT_STUB_CLASS(p); 
+  packetGenStub = new PACKETGENRRR_CLIENT_STUB_CLASS(p); 
+}
+
+// destructor
+AIRBLUE_DRIVER_CLASS::~AIRBLUE_DRIVER_CLASS()
+{
+}
+
+// init
+void
+AIRBLUE_DRIVER_CLASS::Init()
+{
+}
+
+// main
+void
+AIRBLUE_DRIVER_CLASS::Main()
+{
+  int ber,result;
+
+  packetGenStub->SetRate(get_rate());
+  packetGenStub->SetMaxLength(256);
+  packetGenStub->SetMinLength(256);
+  packetGenStub->SetEnable(~0);
+
+  while(1){sleep(5);}
+}
+
+// register driver
+static RegisterDriver<AIRBLUE_DRIVER_CLASS> X;
